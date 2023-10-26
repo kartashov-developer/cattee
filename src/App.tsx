d80de0b4410f7614d163cfb71eb98d5e60217ee4
Vyacheslav Kartashov <kartashov.developer@gmail.com>
2023-10-26 15:06:42 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { LoginPage } from './pages/LoginPage/LoginPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { LoginPage, RegistrationPage, NotFoundPage } from '@/pages';
 
 import './App.css';
 
+const AuthRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/auth" element={<LoginPage />} />
+      <Route path="/registration" element={<LoginPage />} />
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
+  );
+};
+
 function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <AuthRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './LoginPage/LoginPage';
+export * from './RegistrationPage/RegistrationPage';
+export * from './NotFoundPage/NotFoundPage';
