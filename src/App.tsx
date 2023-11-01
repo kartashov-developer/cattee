b0bdb42b6b5a9a1d160aa15a167a7ea92b21c735
Vyacheslav Kartashov <vk@MacBook-Air-Vyacheslav.local>
2023-11-01 22:22:36 +0300

    feat: added AuthRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { LoginPage, RegistrationPage, NotFoundPage } from '@pages/index';
 
 import './App.css';
 
+/*  */
 const AuthRoutes = () => {
   return (
     <Routes>
       <Route path="/auth" element={<LoginPage />} />
       <Route path="/registration" element={<RegistrationPage />} />
+      <Route path="*" element={<Navigate to="/auth" />} />
+    </Routes>
+  );
+};
+
+const MainRoutes = () => {
+  return (
+    <Routes>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
-    <BrowserRouter>
-      <AuthRoutes />
-    </BrowserRouter>
+    <BrowserRouter>{isAuth ? <MainRoutes /> : <AuthRoutes />}</BrowserRouter>
   );
 }
 
